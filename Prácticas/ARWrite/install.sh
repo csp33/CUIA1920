c36fdab8430fa878a5069cba66b49f4a1ad016f9
Carlos Sánchez <karlossanpa@gmail.com>
2020-04-23 17:33:14 +0200

    Missing dependency

--- Prácticas/ARWrite/install.sh
+++ Prácticas/ARWrite/install.sh
@@ -2,7 +2,7 @@
 (
 # =================================================================
 echo "# Installing python packages" ;
-pkexec sudo apt-get install -y python3 python3-pip;
+pkexec sudo apt-get install -y python3 python3-pip python3-tk;
 
 # =================================================================
 echo "25"
